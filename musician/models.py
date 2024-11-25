ec2acee0a5e45c91513adc53989bdcb6f9bc6d1a
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-25 16:20:01 +0200

    Add Musician model and initial migration
    - Implemented the Musician model with __str__ method, age validation and property.
    - Generated initial migration.

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,21 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField()
+    date_of_applying = models.DateField(auto_now_add=True)
+
+    @property
+    def is_adult(self):
+        return self.age >= 21
+
+    def clean(self):
+        if self.age < 14:
+            raise ValidationError("Musicians must be at least 14 years old.")
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
