326d1b171dfc662e3d07a28fa3e97e9f393974c8
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-25 16:03:42 +0200

    Update TIME_ZONE and reorganize installed apps
    - Changed TIME_ZONE setting from "UTC" to "Europe/Kiev".
    - Grouped third-party and user-defined apps in the installed apps section.

--- music_school/settings.py
+++ music_school/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #3-rd apps:
     "rest_framework",
-    "musician",
+    #user apps:
+    "musician"
 ]
 
 MIDDLEWARE = [
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
