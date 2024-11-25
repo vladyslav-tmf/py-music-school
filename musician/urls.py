c3f0ff0010203dcf70f2f1ca08288ec65d92d657
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-25 16:46:03 +0200

    Add Musician ViewSet and API endpoints
    - Implement a "MusicianViewSet" to handle CRUD operations for the Musician model.
    - Updated "musician/urls.py" to register the ViewSet with a REST framework router, providing necessary URL routing for the API endpoints.

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from musician.views import MusicianViewSet
+
+router = DefaultRouter()
+router.register("musicians", MusicianViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "musician"
